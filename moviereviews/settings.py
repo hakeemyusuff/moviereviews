0bc7284d9856fd196dbeb864f26eadf1efd02d5b
Yusuff Abdulhakeem <hakeemyusuff19@gmail.com>
2024-08-01 17:41:38 +0100

    changed database to sqlite

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -79,11 +79,8 @@ WSGI_APPLICATION = "moviereviews.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "moviereviews",
-        "USER": "root",
-        "PASSWORD": "",
-        "HOST": "localhost",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
